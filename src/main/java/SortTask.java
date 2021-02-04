0b24de4b6b13c097a20f0543ac4551cf95960eab
a.kascheev <tony.kascheev@yandex.ru>
2021-02-04 19:00:44 +0300

    Ctrl+alt+L

--- src/main/java/SortTask.java
+++ src/main/java/SortTask.java
@@ -7,35 +7,36 @@ public class SortTask {
         persons.add(new Person("Sofia ", 29));
         persons.add(new Person("Bilbo ", 99));
         persons.add(new Person("Veldemar ", 12));
-        persons.add(new Person("Igness " , 22));
-        persons.add(new Person("Ivan ",40));
-        persons.add(new Person("Jack ",16));
-        persons.add(new Person("Hendrick",75));
+        persons.add(new Person("Igness ", 22));
+        persons.add(new Person("Ivan ", 40));
+        persons.add(new Person("Jack ", 16));
+        persons.add(new Person("Hendrick", 75));
 
-        persons.stream().filter(person -> person.getAge()<30).sorted(Comparator.comparing(Person::getAge))
+        persons.stream().filter(person -> person.getAge() < 30).sorted(Comparator.comparing(Person::getAge))
                 .forEach(System.out::println);
-
 
 
     }
 }
-
 
 class Person {
 
     String fname;
 
     int age;
+
     public int getAge() {
         return age;
     }
-    public Person(String fname,  int age) {
+
+    public Person(String fname, int age) {
         this.fname = fname;
 
         this.age = age;
     }
+
     @Override
     public String toString() {
-        return fname  + age;
+        return fname + age;
     }
 }
